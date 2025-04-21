fa06655d9fa399938fc7ccd25ed02e36a3d5c9d6
IT308 <IT308@DESKTOP-2LCMQUP>
2025-04-21 15:55:27 +0900

    수정

--- mid_exam/exam.cpp
+++ mid_exam/exam.cpp
@@ -16,25 +16,21 @@ public:
     ~Stack() { delete[] mem; } // 배열 반환
     Stack& operator << (int x) {
         if (tos == size - 1) {
-            cout << "스택이 가득 찼습니다!" << endl;
-            return *this; // 스택이 가득 차면 현재 스택 객체 반환
+            cout << "스택이 가득 참" << endl;
+            return *this;
         }
         tos++;
         mem[tos] = x;
-        return *this; // 연속적인 푸시를 위해 스택 객체 자신을 반환
+        return *this;
     }
-
-    // 팝 (pop) 연산자 >>
     Stack& operator >> (int& x) {
         if (tos == -1) {
-            cout << "스택이 비어 있습니다!" << endl;
-            // 스택이 비어있을 경우 x 값을 어떻게 처리할지 결정해야 합니다.
-            // 여기서는 변경하지 않고 스택 객체를 반환합니다.
+            cout << "스택이 비어있음" << endl;
             return *this;
         }
         x = mem[tos];
         tos--;
-        return *this; // 연속적인 팝을 위해 스택 객체 자신을 반환
+        return *this;
     }
     bool operator ! () {
         return this->tos == -1 ? true : false;
